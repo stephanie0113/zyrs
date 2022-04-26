915c8b406f8478893d111ab0aef60ade0d29a8f8
chengyong02 <cyln1208@163.com>
2022-04-26 23:01:27 +0800

    公共js提取

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-﻿
-
-;(function() {
+﻿;(function() {
     if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
         handleFontSize();
     } else {
@@ -18,4 +16,33 @@
             WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
         });
     }
+  //  头部导航点击实践
+  $(".togoNav").click(function (){
+    $('.navigation').toggle(300)
+    $(this).hide().siblings().show()
+    $('.gray').toggle()
+  })
+  $(".gray").click(function (){
+    $(this).hide()
+    $('.navigation').toggle(300)
+  })
+  $('.operation').on('click','span',function (){
+    $(this).addClass('active').siblings().removeClass('active')
+  })
+  $('.md_right').on('click','.language',function (){
+    $(this).find('i').addClass('active').parent().siblings().find('i').removeClass('active')
+  })
+  $(".navigation").on('click','.clas_a',function (){
+    if($(this).hasClass('has_ol')) {
+      $(this).find('.right_case').toggle(300)
+      $(this).siblings().find('.right_case').hide(300)
+      $(this).toggleClass('rote')
+    } else {
+      $(this).addClass('active').siblings().removeClass('active rote').find('.clas_b').removeClass('active').parents('.right_case').hide(300)
+    }
+  }).on('click','.clas_b',function (e){
+    e.stopPropagation();
+    $(this).addClass('active').siblings().removeClass('active').parents('.clas_a').siblings().removeClass('active').find('.clas_b').removeClass('active')
+  })
+
 })();
